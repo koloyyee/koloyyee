e6003c46569fddb6781915d36cf9f6fe0684f6ee
Loy Yee Ko <koloyyee@gmail.com>
2025-12-24 12:57:59 -0500

    fix: add url back to editPost

--- src/config.ts
+++ src/config.ts
@@ -14,9 +14,9 @@ export const SITE = {
   showArchives: true,
   showBackButton: true, // show back button in post detail
   editPost: {
-    enabled: true,
+    enabled: false,
     text: "Edit page",
-    //url: "https://github.com/satnaing/astro-paper/edit/main/",
+    url: "https://github.com/koloyyee/koloyyee/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
